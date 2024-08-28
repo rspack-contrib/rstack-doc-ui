0741e39d4d9bc0ddda5258d5ef26e3a32c9cb662
Soon <a1149625757@126.com>
2024-08-28 17:12:32 +0800

    feat: add web-infra-logo svg and add addon-themes for inspect (#4)
    * feat: add web-infra-logo svg and add dark mode addon
    
    * chore: remove

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,10 @@ import type { StorybookConfig } from 'storybook-react-rsbuild';
 const config: StorybookConfig = {
   framework: 'storybook-react-rsbuild',
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-themes',
+  ],
 };
 
 export default config;
