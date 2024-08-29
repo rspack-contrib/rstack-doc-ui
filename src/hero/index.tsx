7ac0d7f66f7878db2c60c6c84e6bb33e8de2ad25
sunyiteng <sunyiteng@bytedance.com>
2024-08-29 10:58:47 +0800

    chore: add hero

--- src/hero/index.tsx
+++ src/hero/index.tsx
@@ -92,6 +92,7 @@ export const Hero: FC<HeroProps> = ({
           ? positions.map(([top, left], i) => {
               return (
                 <BackgroundStar
+                  // biome-ignore lint/suspicious/noArrayIndexKey: const array is fixed
                   key={i}
                   top={`${top}%`}
                   left={`${left}%`}
