13afd3f1a75b765fe2f70c083d87bddd7f04d5d8
chenjiahan <chenjiahan.jait@bytedance.com>
2024-09-09 22:40:24 +0800

    feat: add announcement component

---
+++ stories/Announcement.stories.tsx
@@ -0,0 +1,18 @@
+import { Announcement } from '@rstack-dev/doc-ui/announcement';
+import './index.scss';
+
+export const AnnouncementStory = () => (
+  <>
+    <div style={{ margin: 20 }}>
+      <Announcement
+        href="https://rspack.dev"
+        message="This is a test announcement"
+        localStorageKey="test-announcement"
+      />
+    </div>
+  </>
+);
+
+export default {
+  title: 'Announcement',
+};
