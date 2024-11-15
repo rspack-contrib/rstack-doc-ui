8e442624b96840ee8678cb977efaed1243845f56
neverland <chenjiahan.jait@bytedance.com>
2024-11-15 15:59:26 +0800

    refactor(build): switch to Rslib

---
+++ rslib.config.ts
@@ -0,0 +1,43 @@
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSass } from '@rsbuild/plugin-sass';
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  plugins: [pluginReact(), pluginSass()],
+  lib: [
+    {
+      format: 'esm',
+      syntax: 'es2015',
+      autoExternal: false,
+      banner: {
+        js: `import './index.css';`,
+      },
+      dts: true,
+      source: {
+        entry: {
+          'nav-icon': './src/nav-icon/index.tsx',
+          benchmark: './src/benchmark/index.tsx',
+          'tool-stack': './src/tool-stack/index.tsx',
+          hero: './src/hero/index.tsx',
+          'section-style': './src/section-style/index.tsx',
+          'fully-featured': './src/fully-featured/index.tsx',
+          'built-with-rspack': './src/built-with-rspack/index.tsx',
+          'why-rspack': './src/why-rspack/index.tsx',
+          'background-image': './src/background-image/index.tsx',
+          announcement: './src/announcement/index.tsx',
+        },
+      },
+    },
+  ],
+  output: {
+    target: 'web',
+    externals: ['react', 'react-dom', 'react/jsx-runtime', 'framer-motion'],
+    filename: {
+      js: '[name]/index.js',
+      css: '[name]/index.css',
+    },
+    distPath: {
+      css: '',
+    },
+  },
+});
