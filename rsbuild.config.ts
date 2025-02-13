6510fb3d60b40f3a444cbd3517931834eaae0040
fi3ework <fi3ework@gmail.com>
2025-02-13 20:12:12 +0800

    chore: clean build tools

--- rsbuild.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginSass } from '@rsbuild/plugin-sass';
-
-export default defineConfig({
-  plugins: [pluginReact(), pluginSass()],
-});
