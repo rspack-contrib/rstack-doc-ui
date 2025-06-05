a0e64b7fe4d4770e4b6ea3816c9c4b70a8bfbc70
neverland <chenjiahan.jait@bytedance.com>
2025-06-05 18:52:27 +0800

    chore(deps): update Storybook v9 (#41)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,7 @@ import type { StorybookConfig } from 'storybook-react-rsbuild';
 const config: StorybookConfig = {
   framework: 'storybook-react-rsbuild',
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-themes',
-    'storybook-addon-rslib',
-  ],
+  addons: ['@storybook/addon-themes', 'storybook-addon-rslib'],
 };
 
 export default config;
